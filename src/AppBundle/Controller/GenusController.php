1d2780237fa53adff3799c40f096c5b2b7f17479
Ibrahim <ibrahim.j@sync.com.lb>
2019-04-05 14:00:43 +0300

    Listing and Using Services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -13,12 +13,10 @@ class GenusController extends Controller{
      */
     public function showAction($genusName){
 
-        $templating=$this->container->get('templating');
-        $html=$templating->render('genus/show.html.twig',[
+        return $this->render('genus/show.html.twig',[
             'name'=>$genusName
         ]);
-
-        return new Response($html); 
+    
     }
 }
 
