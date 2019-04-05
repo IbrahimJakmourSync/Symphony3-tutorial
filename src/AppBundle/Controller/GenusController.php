b76c705b61c0ce159b83c4e89ab32243e8572f21
Ibrahim <ibrahim.j@sync.com.lb>
2019-04-05 13:47:16 +0300

    first page + routing wildcards

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GenusController extends Controller{
+
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName){
+
+        $templating=$this->container->get('templating');
+        $html=$templating->render('genus/show.html.twig',[
+            'name'=>$genusName
+        ]);
+
+        return new Response($html); 
+    }
+}
+
+?>
